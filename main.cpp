5e7c910c9d1a3f98a488c74590d8ceef0beda4da
Niamh <Cilliangrimesmcdonough@gmail.com>
2026-06-02 14:25:06 +0000

    Added comments of my thought process parsing the tutorial I follow.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// Made from a tutorial on geeksforgeeks.org, I added comments of my understanding of the code.
+// Code is initialized
 int main() {
     char op;
     double a, b, res;
+    // Variables are declared
 
     cout << "Enter an operator (+, -, *, /): ";
+    // cout takes an input and displays a message to the user
     cin >> op;
+    // cin maps the input that's brought in to a variable
     cout << "Enter two numbers: ";
     cin >> a >> b;
+    // Varaibles can be split
 
+    // Swith performs different action on a variable depending on it's value
     switch (op) {
         case '+' :
             res = a + b;
@@ -26,11 +32,14 @@ int main() {
         default:
             cout << "Error! Operator is not correct";
             res = -DBL_MAX;
+            // DBL_MAX is the maximum floating point number in C++ so minusing it leads to an incredibly small number
+            // I'm unusre why it is used in this case as it seems any number would work.
     }
 
     if (res != -DBL_MAX) 
         cout << "Result: " << res;
     return 0;
+    // If the program has a valid operator it will output the result.
    
 }
     
